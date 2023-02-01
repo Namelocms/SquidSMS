238485916a5de557603117b0cbbc62cf562d579b
Sean Coleman <103694350+Namelocms@users.noreply.github.com>
2023-01-31 23:35:34 -0700

    Update sendSMS.py
    Added connect, disconnect, exception handling, some comments

--- sendSMS.py
+++ sendSMS.py
@@ -33,48 +33,96 @@ class SendSMS:
 
 		# App Password (not email password) for 'AUTH_EMAIL'
 		self.AUTH_PASS = password
+		
+		# Email + Password used to send message
+		self.auth = (self.AUTH_EMAIL, self.AUTH_PASS)
+	
+	# Connect to the SMTP server
+	def connect(self):
+		try:
+			# Establish a secure session with gmail's outgoing SMTP server using your gmail account
+			server = smt.SMTP("smtp.gmail.com", 587)
+			server.starttls()
+
+			if server.login(self.auth[0], self.auth[1]):
+				print(Fore.GREEN + 'Login Successful' + Style.RESET_ALL)
+				return server
+
+			else:
+				raise smt.SMTPAuthenticationError
+
+		except smt.SMTPAuthenticationError:
+			print(Fore.RED + Style.BRIGHT)
+			print('===============================================================================================')
+			print('SMS SEND FAILED:\nError: SMTPAuthenticationError: Email/Password Incorrect\n'
+				  'Email:\n  {}\n'
+				  'Date:\n  {}'.format(self.AUTH_EMAIL, dt.datetime.now()))
+			print('===============================================================================================')
+
+			# self.server.quit()  # end server connection
+			print(Fore.RED + Style.BRIGHT + 'SMTP Server Connection Ended!')
+			print(Style.RESET_ALL)
+
+			return server.quit()  # end server connection
 
 	def send(self, phone_number, carrier, message):
-		try:
-			if not self.check_phone_number(phone_number) or not self.check_carrier(carrier):
-				raise ValueError
-			else:
-				# convert phone_number and carrier into usable string
-				recipient = "{} {}".format(phone_number, self.CARRIERS[carrier])
+		# server is returned as type tuple() on disconnect, so if not disconnected, continue
+		if type(server) is not tuple:	
+			try:
+				if not self.check_phone_number(phone_number) or not self.check_carrier(carrier):
+					raise ValueError
+				else:
+					# convert phone_number and carrier into usable string
+					recipient = "{} {}".format(phone_number, self.CARRIERS[carrier])
+					
+					try:
+						# Send text message through SMS gateway of destination number
+						server.sendmail(auth[0], recipient, message)
+						print(Fore.GREEN + 'Send Successful' + Style.RESET_ALL)
+						return True
 
-				# Email + Password used to send message
-				auth = (self.AUTH_EMAIL, self.AUTH_PASS)
+					except (smt.SMTPAuthenticationError, AttributeError):
+						print(Fore.RED + Style.BRIGHT)
+						print('===============================================================================================')
+						print('SMS SEND FAILED:\nError: Email/Password Incorrect\n'
+							  'Email:\n  {}\n'
+							  'Password:\n  {}\n'
+							  'Recipient:\n  {}::{}\n'
+							  'Message:\n  {}\n'
+							  'Date:\n  {}'.format(self.AUTH_EMAIL, self.AUTH_PASS, phone_number, carrier, message, dt.datetime.now()))
+						print('===============================================================================================')
+						print(Style.RESET_ALL)
+						
+						# In case connection is ended already
+						try:
+							self.disconnect(server)
+						except AttributeError:
+							pass
+						return False
 
-				try:
-					# Establish a secure session with gmail's outgoing SMTP server using your gmail account
-					server = smt.SMTP("smtp.gmail.com", 587)
-					server.starttls()
-					if server.login(auth[0], auth[1]):
-						print(Fore.GREEN + 'Login Successful' + Style.RESET_ALL)
-					else:
-						raise smt.SMTPAuthenticationError
-
-					# Send text message through SMS gateway of destination number
-					server.sendmail(auth[0], recipient, message)
-					print(Fore.GREEN + 'Send Successful' + Style.RESET_ALL)
-
-				except smt.SMTPAuthenticationError:
-					print(Fore.RED + Style.BRIGHT)
-					print('===============================================================================================')
-					print('SMS SEND FAILED:\nError: SMTPAuthenticationError: Email/Password Incorrect\n'
-						  'Email:\n  {}\n'
-						  'Recipient:\n  {}::{}\n'
-						  'Message:\n  {}\n'
-						  'Date:\n  {}'.format(self.AUTH_EMAIL, phone_number, carrier, message, dt.datetime.now()))
-					print('===============================================================================================')
-					print(Style.RESET_ALL)
-					return False
-
-		except ValueError:
-			print(Fore.RED + 'Invalid Recipient/Carrier Information')
+			except ValueError:
+				print(Fore.RED + 'Invalid Recipient/Carrier Information')
+				# In case connection is ended already
+						try:
+							self.disconnect(server)
+						except AttributeError:
+							pass
+						return False
+		else:
+			# In case connection is ended already
+			try:
+				self.disconnect(server)
+			except AttributeError:
+				pass
+			
 			return False
-
-		return True
+		
+	# end server connection
+	@staticmethod
+	def disconnect(server):
+		server.quit()  # end server connection
+		print(Fore.RED + Style.BRIGHT + 'SMTP Server Connection Ended!')
+		print(Style.RESET_ALL)
 
 	# Ensure provided number formatting is correct,
 	# wont check if number is real or not
